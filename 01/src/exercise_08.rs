aee7513cfad6008e5ff393218298100b138077c1
Jared McDonald <jared.mcdonald@nytimes.com>
2017-09-27 18:10:29 -0400

    toying with more hamming distances, this is definitely not right

--- 01/src/exercise_08.rs
+++ 01/src/exercise_08.rs
@@ -4,7 +4,21 @@ use edit_distance::get_edit_distance;
 use aes::decrypt_aes_ecb;
 
 fn is_aes_encrypted(bytes: &[u8]) -> bool {
-    // ... todo, probably something about edit distances?
+    let keys = [
+        vec![0; 16],
+        vec![1; 16],
+        vec![2; 16],
+        vec![3; 16],
+        vec![4; 16],
+    ];
+
+    // wattttt
+    let decryption_attempts = keys.iter().map(|key| decrypt_aes_ecb(&bytes, key)).collect::<Vec<_>>();
+    let distance_1 = get_edit_distance(&decryption_attempts[0], &decryption_attempts[1]) as i32;
+    let distance_2 = get_edit_distance(&decryption_attempts[1], &decryption_attempts[2]) as i32;
+    let distance_3 = get_edit_distance(&decryption_attempts[2], &decryption_attempts[3]) as i32;
+    let distance_4 = get_edit_distance(&decryption_attempts[3], &decryption_attempts[4]) as i32;
+
     false
 }
 
