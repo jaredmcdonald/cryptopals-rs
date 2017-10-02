bbba437fa91fb815a61bba39172e47d413eac181
Jared McDonald <jared.r.mcdonald@gmail.com>
2017-10-02 01:43:05 -0400

    solve 2.12

--- src/exercise_12.rs
+++ src/exercise_12.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
-use aes::encrypt_aes_ecb;
-use aes_oracles::{random_key, detection_oracle};
 use base64::decode as base64_decode;
+use aes::encrypt_aes_ecb;
+use aes_oracles::{random_key, detection_oracle, AesEncryptionMode};
+use ascii::bytes_to_ascii_string;
 
 fn create_encrypter() -> Box<Fn(&[u8]) -> Vec<u8>> {
     let key = random_key();
@@ -16,9 +17,8 @@ fn create_encrypter() -> Box<Fn(&[u8]) -> Vec<u8>> {
 
 fn find_blocksize(encrypter: &Box<Fn(&[u8]) -> Vec<u8>>) -> usize {
     let mut blocksize = 0;
-    let byte = 0x0;
-    let smallest_size = encrypter(vec![byte; 0x1].as_slice()).len();
-    for n in 0x002..0x100 {
+    let smallest_size = encrypter(Vec::new().as_slice()).len();
+    for n in 0x001..0x100 {
         let result = encrypter(vec![0x0u8; n].as_slice());
         let len = result.len();
         if len != smallest_size {
@@ -29,23 +29,49 @@ fn find_blocksize(encrypter: &Box<Fn(&[u8]) -> Vec<u8>>) -> usize {
     blocksize
 }
 
-fn find_first_byte(encrypter: &Box<Fn(&[u8]) -> Vec<u8>>, blocksize: usize) -> u8 {
+fn decrypt_byte(
+    encrypter: &Box<Fn(&[u8]) -> Vec<u8>>,
+    len_with_padding: usize,
+    decoded_bytes: &[u8],
+    target_index: usize
+) -> Option<u8> {
     let mut first_blocks_to_bytes = HashMap::new();
-    let short_block = vec![0x0; blocksize - 1];
+    let short_block = vec![0x0; target_index];
+    let mut partially_decoded = short_block.clone();
+    partially_decoded.extend(decoded_bytes);
     for byte in 0..0xff {
-        let mut input = short_block.clone();
+        let mut input = partially_decoded.clone();
         input.push(byte);
-        let first_output_block = encrypter(input.as_slice())[..blocksize].to_vec();
+        let first_output_block = encrypter(input.as_slice())[..len_with_padding].to_vec();
         first_blocks_to_bytes.insert(first_output_block, byte);
     }
-    let actual_first_byte = encrypter(short_block.as_slice())[..blocksize].to_vec();
-    *first_blocks_to_bytes.get(&actual_first_byte).unwrap()
+    let actual_first_byte = match encrypter(&short_block).get(..len_with_padding) {
+        Some(byte) => byte.to_vec(),
+        None => return None,
+    };
+    Some(*first_blocks_to_bytes.get(&actual_first_byte).unwrap())
+}
+
+fn decrypt_ecb(encrypter: &Box<Fn(&[u8]) -> Vec<u8>>, blocksize: usize) -> Vec<u8> {
+    let unknown_str_len = encrypter(vec![].as_slice()).len() + blocksize; // add blocksize for padding
+    let mut decrypted_bytes = Vec::new();
+    for byte_n in 0..unknown_str_len {
+        let num_unknowns = unknown_str_len - (byte_n + 1);
+        let partially_decoded = decrypted_bytes.clone();
+        match decrypt_byte(&encrypter, unknown_str_len, partially_decoded.as_slice(), num_unknowns) {
+            Some(byte) => decrypted_bytes.push(byte),
+            None => break,
+        }
+    }
+    decrypted_bytes
 }
 
 pub fn run_12() {
     let encrypter = create_encrypter();
     let blocksize = find_blocksize(&encrypter);
-    let mode = detection_oracle(&encrypter);
-    let first_byte = find_first_byte(&encrypter, blocksize);
-    println!("{}, {:?}", first_byte, mode);
+    let decrypted = match detection_oracle(&encrypter) {
+        AesEncryptionMode::ECB => decrypt_ecb(&encrypter, blocksize),
+        _ => panic!("it's not ECB, I don't know how to decrypt this!"),
+    };
+    println!("decrypted unknown-string:\n{}", bytes_to_ascii_string(&decrypted));
 }
