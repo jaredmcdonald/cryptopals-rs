fb6476bcc6d97d8c4d4525d32074efff5c7514f7
Jared McDonald <jared.r.mcdonald@gmail.com>
2018-01-10 17:19:51 -0500

    this seems more right than before

--- src/exercise_17.rs
+++ src/exercise_17.rs
@@ -7,8 +7,8 @@ use utils::as_blocks;
 
 fn encrypter(key: &[u8], iv: &[u8]) -> Vec<u8> {
     let b64_strings = [
-        "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
-        // "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+        // "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+        "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
         // "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
         // "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
         // "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
@@ -25,36 +25,37 @@ fn encrypter(key: &[u8], iv: &[u8]) -> Vec<u8> {
 fn decrypt_block(
     block: &[u8],
     previous_block: &[u8],
-    padding_oracle: &Fn(&[u8]) -> bool
-) -> Option<u8> {
+    padding_oracle: &Fn(&[u8], &[u8]) -> bool
+) -> [u8; BLOCK_SIZE] {
     let faux_block = random_bytes(BLOCK_SIZE);
-    for byte in 0x0..0xff {
-        let mut faux_ciphertext = faux_block.clone();
-        faux_ciphertext[BLOCK_SIZE - 1] = byte;
-        faux_ciphertext.extend(block);
-        if padding_oracle(&faux_ciphertext) {
+    let mut found = [0u8; BLOCK_SIZE];
+    for byte in 0x1..0xff {
+        let mut faux_iv = faux_block.clone();
+        faux_iv[BLOCK_SIZE - 1] = byte;
+
+        if padding_oracle(&faux_iv, block) {
             // ? todo
             let intermediate_byte = byte ^ 1;
-            return Some(previous_block[15] ^ intermediate_byte);
+            found[BLOCK_SIZE - 1] = previous_block[BLOCK_SIZE - 1] ^ intermediate_byte;
         }
     }
-    None
+    found
 }
 
 pub fn run_17() {
     let key = random_bytes(BLOCK_SIZE);
-    let iv = random_bytes(BLOCK_SIZE);
-    let ciphertext = encrypter(&key, &iv);
+    let real_iv = random_bytes(BLOCK_SIZE);
+    let ciphertext = encrypter(&key, &real_iv);
     let len = ciphertext.len();
 
-    let padding_oracle = |ciphertext: &[u8]| -> bool {
+    let padding_oracle = |iv: &[u8], ciphertext: &[u8]| -> bool {
         let decrypted = decrypt_aes_cbc(ciphertext, &key, &iv);
         unpad(&decrypted, BLOCK_SIZE).is_ok()
     };
 
-    println!("{}", decrypt_block(
+    println!("{:?}", decrypt_block(
         &ciphertext[len - BLOCK_SIZE..],
         &ciphertext[len - BLOCK_SIZE * 2..len - BLOCK_SIZE],
         &padding_oracle
-    ).unwrap());
+    ));
 }
