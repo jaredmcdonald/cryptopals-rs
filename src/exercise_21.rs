e197da0843489844c590d65e797bb253f42b5c18
Jared McDonald <jared.r.mcdonald@gmail.com>
2018-01-17 14:44:22 -0500

    3.21 matches python implementation i found on the internet, trying to understand better

--- src/exercise_21.rs
+++ src/exercise_21.rs
@@ -2,16 +2,16 @@
 
 const W: usize = 32;
 const N: usize = 624;
-// const M: usize = 397;
+const M: usize = 397;
 // const R: usize = 31;
-// const A: usize = 0x9908b0df;
-// const U: usize = 11;
+const A: u32 = 0x9908b0df;
+const U: u32 = 11;
 const D: u32 = 0xffffffff;
-// const S: usize = 7;
-// const B: usize = 0x9d2c5680;
-// const T: usize = 15;
-// const C: usize = 0xefc60000;
-// const L: usize = 18;
+const S: u32 = 7;
+const B: u32 = 0x9d2c5680;
+const T: u32 = 15;
+const C: u32 = 0xefc60000;
+const L: u32 = 18;
 const F: u32 = 1812433253;
 
 // https://en.wikipedia.org/wiki/Mersenne_Twister
@@ -38,11 +38,36 @@ impl MersenneTwister {
             let n = F.wrapping_mul(prev ^ (prev >> (W - 2))) + i as u32;
             self.state.push(n);
         }
-        println!("{:?}", self.state);
+    }
+
+    pub fn rand(&mut self) -> u32 {
+        if self.index == 0 {
+            self.generate();
+        }
+        let mut y = self.state[self.index];
+        y ^= y >> U;
+        y ^= (y << S) & B;
+        y ^= (y << T) & C;
+        y ^= y >> L;
+
+        self.index = (self.index + 1) % N;
+        y
+    }
+
+    fn generate(&mut self) {
+        for i in 0..N {
+            let mut n = self.state[i] & 0x80000000;    // wat is this number?
+            n += self.state[(i + 1) % N] & 0x7fffffff; // and this one?
+            self.state[i] = self.state[(i + M) % N] ^ (n >> 1);
+            if n % 2 != 0 {
+                self.state[i] ^= A;
+            }
+        }
     }
 }
 
 pub fn run_21() {
     let mut rng = MersenneTwister::new();
     rng.seed(1);
+    println!("{}\n{}\n{}", rng.rand(), rng.rand(), rng.rand());
 }
